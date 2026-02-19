59340c61fe2a53b35c11ed5cb9b34dff11c66d8b
Ali Amir <codepirate2028@gmail.com>
2026-02-19 17:41:07 -0500

    Remove forward declaration of Grader class
    Removed forward declaration for Grader class.

--- cache.h
+++ cache.h
@@ -4,7 +4,6 @@
 #include <string>
 #include "math.h"
 using namespace std;
-class Grader;   // forward declaration, will be used for grdaing
 class Tester;   // forward declaration, will be used for testing
 class Person;   // forward declaration
 class Cache;    // forward declaration
